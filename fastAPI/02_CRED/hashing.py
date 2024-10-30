c14d73e658d25dde863c33f39c939dc2a4107c54
AravInd PB <43701328+thedemonKingx1337@users.noreply.github.com>
2024-10-31 01:14:42 +0530

    JWT Uping

--- fastAPI/02_CRED/hashing.py
+++ fastAPI/02_CRED/hashing.py
@@ -8,3 +8,6 @@ class Hash():
     def bcrypt(password: str):
         hashed_password = pwd_cxt.hash(password)
         return hashed_password
+
+    def verify(hashed_password_from_db: str, password: str):
+        return pwd_cxt.verify(password, hashed_password_from_db)
