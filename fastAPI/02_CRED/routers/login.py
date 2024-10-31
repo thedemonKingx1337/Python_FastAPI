9f2b3196b3d8349a548cbc656ada48568d8d1055
AravInd PB <43701328+thedemonKingx1337@users.noreply.github.com>
2024-10-31 19:26:09 +0530

    Complete Bug Fixed

--- fastAPI/02_CRED/routers/login.py
+++ fastAPI/02_CRED/routers/login.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from datetime import timedelta
-
+from fastapi.security import OAuth2PasswordRequestForm
 
 from ..model import schemas, tableModels
 from ..database import database
@@ -15,7 +15,7 @@ router = APIRouter(
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(tableModels.User).filter(
         tableModels.User.email == request.username).first()
     if not user:
